470c491bbc3bd0ee041c9ef5060063a8d8b1e7a0
huanggz <918031078@qq.com>
2022-07-15 13:48:24 +0800

    flag的基本使用

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
-import "go-public-chain/blc"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	// 创世区块
-	blockChain := blc.CreateBlockChainWithGenesisBlock()
-	defer blockChain.DB.Close()
+	// // 创世区块
+	// blockChain := blc.CreateBlockChainWithGenesisBlock()
+	// defer blockChain.DB.Close()
 
-	// 新区块
-	blockChain.AddBlockToBlockChain("send 100rmb to zhansan")
-	blockChain.AddBlockToBlockChain("send 400rmb to lisi")
-	blockChain.AddBlockToBlockChain("send 300rmb to wangwu")
+	// // 新区块
+	// blockChain.AddBlockToBlockChain("send 100rmb to zhansan")
+	// blockChain.AddBlockToBlockChain("send 400rmb to lisi")
+	// blockChain.AddBlockToBlockChain("send 300rmb to wangwu")
 
-	// 遍历区块
-	blockChain.PrintChain()
+	// // 遍历区块
+	// blockChain.PrintChain()
+	flagPrintChainCmd := flag.String("printchain", "", "输出所有区块")
+	flag.Parse()
+	fmt.Printf("%s\n", *flagPrintChainCmd)
 }
