7a176c24b6a96e954b2a69f963dda4666fa5bb37
huanggz <918031078@qq.com>
2022-07-15 08:52:47 +0800

    数据库的创建，打开

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-public-chain
 
 go 1.17
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
+)
--- main.go
+++ main.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"go-public-chain/blc"
+	"log"
+
+	"github.com/boltdb/bolt"
 )
 
 func main() {
-	// // 创世区块
-	// blockChain := blc.CreateBlockChainWithGenesisBlock()
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// // 新区块
-	// blockChain.AddBlockToBlockChain("Send 100Rmb To zhangshan", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
-	// blockChain.AddBlockToBlockChain("Send 200Rmb To hah", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
-
-	// fmt.Println(blockChain)
-	// fmt.Println(blockChain.Blocks)
-
-	block := blc.NewBlock("Test", 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
-	fmt.Printf("%d\n", block.Nonce)
-	fmt.Printf("%x\n", block.Hash)
-
-	bytes := block.Serialize()
-
-	fmt.Println(bytes)
-
-	block = blc.DeserializeBlock(bytes)
-
-	fmt.Printf("%d\n", block.Nonce)
-	fmt.Printf("%x\n", block.Hash)
-
+	defer db.Close()
 }
