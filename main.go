a64c51c298d810b2cc92d704160cdf7552124920
huanggz <918031078@qq.com>
2022-07-15 14:17:37 +0800

    命令行解析

--- main.go
+++ main.go
@@ -1,23 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("\taddblock -data DATA -- 交易数据")
+	fmt.Println("\tprintchain -- 输出区块信息")
+}
+
+func isValidArgs() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+}
+
 func main() {
-	// // 创世区块
-	// blockChain := blc.CreateBlockChainWithGenesisBlock()
-	// defer blockChain.DB.Close()
 
-	// // 新区块
-	// blockChain.AddBlockToBlockChain("send 100rmb to zhansan")
-	// blockChain.AddBlockToBlockChain("send 400rmb to lisi")
-	// blockChain.AddBlockToBlockChain("send 300rmb to wangwu")
+	isValidArgs()
 
-	// // 遍历区块
-	// blockChain.PrintChain()
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
 
-	args := os.Args
-	fmt.Printf("%v\n", args)
+	flagAddBlockData := addBlockCmd.String("data", "http://xxx.com", "交易数据")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "printchain":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	default:
+		printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *flagAddBlockData == "" {
+			printUsage()
+			os.Exit(1)
+		}
+
+		fmt.Println(*flagAddBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		fmt.Println("输出所有区块的数据.........")
+	}
 }
