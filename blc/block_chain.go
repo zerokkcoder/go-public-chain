a43e7b7482416afc7d995401c95b4a7cfde5658c
huanggz <918031078@qq.com>
2022-07-15 09:52:16 +0800

    创建创世区块，并将区块存储到数据库中

--- blc/block_chain.go
+++ blc/block_chain.go
@@ -1,20 +1,86 @@
 package blc
 
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
+const dbName = "blockchain.db"  // 数据库名字
+const blockTableName = "blocks" // 表名
+
 type BlockChain struct {
-	Blocks []*Block // 存储有序的区块
+	Tip []byte   // 最新区块的hash
+	DB  *bolt.DB // 数据库
 }
 
 // 1. 创建带有创世区块的区块链
-func CreateBlockChainWithGenesisBlock() *BlockChain {
-	// 创建创世区块
-	genesisBlock := CreateGenesisBlock("Genesis data......")
+func CreateBlockChainWithGenesisBlock() *BlockChain { // 创建或打开数据库
+	db, err := bolt.Open(dbName, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var blockHash []byte
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte(blockTableName))
+		if err != nil {
+			log.Panic(err)
+		}
+		if b != nil {
+			// 创建创世区块
+			genesisBlock := CreateGenesisBlock("Genesis data......")
+			// 将创世区块存储到表中
+			err := b.Put(genesisBlock.Hash, genesisBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			// 存储最新区块的hash
+			err = b.Put([]byte("tip"), genesisBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			blockHash = genesisBlock.Hash
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
 	// 返回区块链对象
-	return &BlockChain{[]*Block{genesisBlock}}
+	return &BlockChain{blockHash, db}
 }
 
 // 2. 增加区块到区块链
 func (bc *BlockChain) AddBlockToBlockChain(data string, height int64, prevHash []byte) {
+	// 创建或打开数据库
+	// db, err := bolt.Open(dbName, 0600, nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer db.Close()
+
+	// err = db.Update(func(tx *bolt.Tx) error {
+	// 	b, err = tx.CreateBucket([]byte(blockTableName))
+	// 	if err != nil {
+	// 		log.Panic(err)
+	// 	}
+	// 	if b != nil {
+
+	// 	}
+
+	// 	return nil
+	// })
+
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
 	// 创建新区块
-	newBlock := NewBlock(data, height, prevHash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	// newBlock := NewBlock(data, height, prevHash)
+	// bc.Blocks = append(bc.Blocks, newBlock)
 }
--- main.go
+++ main.go
@@ -1,60 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"go-public-chain/blc"
-	"log"
-
-	"github.com/boltdb/bolt"
-)
+import "go-public-chain/blc"
 
 func main() {
-	// block := blc.NewBlock("Test", 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
-	// fmt.Printf("%d\n", block.Nonce)
-	// fmt.Printf("%x\n", block.Hash)
-
-	// 创建或打开数据库
-	db, err := bolt.Open("my.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// err = db.Update(func(tx *bolt.Tx) error {
-	// 	b := tx.Bucket([]byte("blocks"))
-	// 	if b == nil {
-	// 		b, err = tx.CreateBucket([]byte("blocks"))
-	// 		if err != nil {
-	// 			log.Panic("Blocks table create fail")
-	// 		}
-	// 	}
-	// 	err = b.Put([]byte("l"), block.Serialize())
-	// 	if err != nil {
-	// 		log.Panic(err)
-	// 	}
-
-	// 	return nil
-	// })
-
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
-
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("blocks"))
-		if b != nil {
-			blockData := b.Get([]byte("l"))
-			// fmt.Println(blockData)
-			// fmt.Printf("%s\n", blockData)
-			block := blc.DeserializeBlock(blockData)
-			fmt.Printf("%v\n", block)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		log.Panic(err)
-	}
-
+	// 创世区块
+	blockChain := blc.CreateBlockChainWithGenesisBlock()
+	defer blockChain.DB.Close()
 }
