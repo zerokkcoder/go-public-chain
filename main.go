583e4409312842b05f5979d4cbdfa6069b762917
huanggz <918031078@qq.com>
2022-07-15 09:23:41 +0800

    区块序列化数据保存到数据库

--- main.go
+++ main.go
@@ -2,38 +2,59 @@ package main
 
 import (
 	"fmt"
+	"go-public-chain/blc"
 	"log"
 
 	"github.com/boltdb/bolt"
 )
 
 func main() {
-	// 创建或者打开数据库
+	// block := blc.NewBlock("Test", 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	// fmt.Printf("%d\n", block.Nonce)
+	// fmt.Printf("%x\n", block.Hash)
+
+	// 创建或打开数据库
 	db, err := bolt.Open("my.db", 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer db.Close()
 
-	// 查看数据
-	err = db.View(func(tx *bolt.Tx) error {
-		// 获取BlockBucket表
-		b := tx.Bucket([]byte("BlockBucket"))
+	// err = db.Update(func(tx *bolt.Tx) error {
+	// 	b := tx.Bucket([]byte("blocks"))
+	// 	if b == nil {
+	// 		b, err = tx.CreateBucket([]byte("blocks"))
+	// 		if err != nil {
+	// 			log.Panic("Blocks table create fail")
+	// 		}
+	// 	}
+	// 	err = b.Put([]byte("l"), block.Serialize())
+	// 	if err != nil {
+	// 		log.Panic(err)
+	// 	}
 
-		// 往表里面存储数据
+	// 	return nil
+	// })
+
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	err = db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("blocks"))
 		if b != nil {
-			data := b.Get([]byte("l"))
-			fmt.Printf("%s\n", data)
-			data = b.Get([]byte("lll"))
-			fmt.Printf("%s\n", data)
+			blockData := b.Get([]byte("l"))
+			// fmt.Println(blockData)
+			// fmt.Printf("%s\n", blockData)
+			block := blc.DeserializeBlock(blockData)
+			fmt.Printf("%v\n", block)
 		}
 
-		// 返回nil，以便数据库处理相应操作
 		return nil
 	})
-	// 更新失败
+
 	if err != nil {
 		log.Panic(err)
 	}
+
 }
