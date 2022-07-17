d46697f1ffff321fe3e7b11bee77f818dabcb013
huanggz <918031078@qq.com>
2022-07-17 12:55:19 +0800

    ripemd160的基本使用

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.17
 
 require (
 	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+
+	"golang.org/x/crypto/ripemd160"
 )
 
 func main() {
 	// cli := blc.CLI{}
 	// cli.Run()
 
-	hasher := sha256.New()
+	// 256
+	// 47cf44ab1c182aeb1660642934317981e4041d5ce994d3f339115361434de879
+	// hasher := sha256.New()
+	// hasher.Write([]byte("huanghuanghuang"))
+	// hashBytes := hasher.Sum(nil)
+	// hashString := fmt.Sprintf("%x", hashBytes)
+	// fmt.Println(hashString)
+
+	// ripemd160
+	// 4c7da9e96f8ec28020b68f6bfe6ffff8e63a3598
+	hasher := ripemd160.New()
 	hasher.Write([]byte("huanghuanghuang"))
 	hashBytes := hasher.Sum(nil)
 	hashString := fmt.Sprintf("%x", hashBytes)
