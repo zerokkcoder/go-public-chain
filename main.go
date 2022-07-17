5c4e29b490ba4de38aa60cbc81e6b0a2ca1e8850
huanggz <918031078@qq.com>
2022-07-17 12:49:48 +0800

    sha256的基本使用

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"go-public-chain/blc"
+	"crypto/sha256"
+	"fmt"
 )
 
 func main() {
-	cli := blc.CLI{}
-	cli.Run()
+	// cli := blc.CLI{}
+	// cli.Run()
+
+	hasher := sha256.New()
+	hasher.Write([]byte("huanghuanghuang"))
+	hashBytes := hasher.Sum(nil)
+	hashString := fmt.Sprintf("%x", hashBytes)
+	fmt.Println(hashString)
 
 }
